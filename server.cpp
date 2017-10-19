9178fc3c75ab9cfeb5d91e54abf03aa85bf43b69
hare1039 <hare39393@gmail.com>
2017-10-20 01:09:29 +0800

    Add simple parsing

--- server.cpp
+++ server.cpp
@@ -6,6 +6,7 @@
 #include <mutex>              // std::mutex, std::unique_lock
 #include <condition_variable> // std::condition_variable
 #include <list>
+#include <regex>
 
 
 #if __cplusplus < 201103L
@@ -76,37 +77,53 @@ int main(int argc, char *argv[])
 	check_error("binding...", err);
 	listen(socketfd, 300);
 
-	std::list<std::thread> chatters;
-	std::mutex internel_conn_mtx;
+	std::list<std::thread>  chatters;
+	std::mutex              internel_conn_mtx;
 	std::condition_variable internel_conn;
+
+
 	for(;;)
 	{
         struct sockaddr_in client_addr;
-        socklen_t addrlen = sizeof(client_addr);
+        socklen_t    addrlen  = sizeof(client_addr);
         unsigned int clientfd = accept(socketfd, (struct sockaddr*)&client_addr, &addrlen);
 
         Chatter gopher(clientfd, client_addr, addrlen);
         chatters.push_back(std::thread([&internel_conn_mtx, &internel_conn, gopher] {
-	        Message message;
+	        Message    message;
 	        std::mutex message_mtx;
+
+
 	        auto comm_handler = [&gopher, &message]{
 		        std::string greet("[Server] Hello, " + gopher.name + "!" +
 		                          " From: " + inet_ntoa(gopher.client_addr.sin_addr) + ":" +
 		                          std::to_string(ntohs(gopher.client_addr.sin_port)) + "\n");
+
+
 		        size_t err = send(gopher.clientfd, greet.c_str(), greet.size(), 0);
 		        check_error("sending...", err);
-				char buf[1000];
+				char        buf[1000];
+		        std::string buf_unlimited;
 				for(;;)
 				{
 					int n = recv(gopher.clientfd, buf, sizeof(buf), 0);
 					check_error("recving...", n, [](int n){return n == 0;});
 					buf[n] = '\0';
-					message.data += buf;
-					auto pos = message.data.find("\n");
+					buf_unlimited += buf;
+					auto pos = buf_unlimited.find("\n");
 					if(pos != std::string::npos)
 					{
+						std::string space = buf_unlimited.substr(0,  pos);
+						buf_unlimited     = buf_unlimited.substr(pos + 1);
 						std::string cmd, arguments;
-						
+						std::cout << "parsing: [" << space << "]\n";
+						if(std::regex_match(space, std::regex(".*\\s.*")))
+						{
+							auto pos  = space.find(" ");
+							cmd       = space.substr(0, pos);
+							arguments = space.substr(pos + 1);
+							std::cout << "rcv cmd: [" << cmd << "], arg:[" << arguments << "]\n";
+						}
 						//switch
 						
 					}
@@ -115,10 +132,13 @@ int main(int argc, char *argv[])
 				std::cout << std::this_thread::get_id() << " exited.\n";
 	        };
 
+	        
 	        auto internel_msg_handler = [&gopher, &message, &internel_conn_mtx, &internel_conn]{
 		        std::unique_lock<std::mutex> lock(internel_conn_mtx);
 		        internel_conn.wait(lock, [&](){return message.receiver == gopher.name;});		        
 	        };
+
+
 	        std::thread conn(comm_handler), inter_msg(internel_msg_handler);
 	        conn.join();
 	        inter_msg.join();
